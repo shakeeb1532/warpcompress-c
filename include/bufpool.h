85e249864284bb0609c61b76d3b741c7c60d5ef3
shakeeb1532 <73624484+shakeeb1532@users.noreply.github.com>
2025-10-21 21:02:55 +1100

    Update bufpool.h

--- include/bufpool.h
+++ include/bufpool.h
@@ -1,11 +1,10 @@
-#ifndef BUFPOOL_H
-#define BUFPOOL_H
+#pragma once
+#include <stddef.h>
 
 typedef struct bufpool bufpool_t;
 
 bufpool_t* pool_create(size_t buf_size, int count);
-void* pool_acquire(bufpool_t* pool);
-void  pool_release(bufpool_t* pool, void* buf);
-void  pool_destroy(bufpool_t* pool);
+void*      pool_acquire(bufpool_t* p);
+void       pool_release(bufpool_t* p, void* buf);
+void       pool_destroy(bufpool_t* p);
 
-#endif
