db47b61a1751aa2f697c2e03e4d583295a7aad14
shakeeb1532 <73624484+shakeeb1532@users.noreply.github.com>
2025-10-21 21:02:20 +1100

    Update threadpool.h

--- include/threadpool.h
+++ include/threadpool.h
@@ -1,10 +1,12 @@
-#ifndef THREADPOOL_H
-#define THREADPOOL_H
+#pragma once
 #include <stddef.h>
-typedef void (*tp_fn)(void *arg);
+
+typedef void (*tp_func_t)(void*);
+
 typedef struct tp tp_t;
+
 tp_t* tp_create(int threads);
-void  tp_submit(tp_t *tp, tp_fn fn, void *arg);
-void  tp_barrier(tp_t *tp);
-void  tp_destroy(tp_t *tp);
-#endif
+void  tp_submit(tp_t* tp, tp_func_t fn, void* arg);
+void  tp_barrier(tp_t* tp);
+void  tp_destroy(tp_t* tp);
+
