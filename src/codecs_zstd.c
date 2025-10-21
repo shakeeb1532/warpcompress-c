d3c5bae1c0af095a738bf8543a2102f5e8d0324d
shakeeb1532 <73624484+shakeeb1532@users.noreply.github.com>
2025-10-21 21:04:26 +1100

    Update codecs_zstd.c

--- src/codecs_zstd.c
+++ src/codecs_zstd.c
@@ -1,8 +1,6 @@
 #include "codecs.h"
-#include <zstd.h>
 #include <stdlib.h>
-
-struct zstd_ctx { ZSTD_CCtx *cctx; };
+#include <string.h>
 
 zstd_ctx_t* zstd_ctx_create(int level, int nb_workers) {
   zstd_ctx_t *c = (zstd_ctx_t*)malloc(sizeof(*c));
@@ -22,14 +20,23 @@ void zstd_ctx_free(zstd_ctx_t* c) {
 
 size_t zstd_compress_ctx(zstd_ctx_t* c, void *dst, size_t dst_cap, const void *src, size_t src_sz) {
   size_t r = ZSTD_compress2(c->cctx, dst, dst_cap, src, src_sz);
-  return ZSTD_isError(r) ? 0 : r;
+  if (ZSTD_isError(r)) return 0;
+  return r;
 }
 
-size_t zstd_max_compressed_size(size_t src_size) {
-  return ZSTD_compressBound(src_size);
+size_t zstd_compress(void *dst, size_t dst_cap, const void *src, size_t src_sz, int level) {
+  size_t r = ZSTD_compress(dst, dst_cap, src, src_sz, level);
+  if (ZSTD_isError(r)) return 0;
+  return r;
 }
 
 size_t zstd_decompress(void *dst, size_t dst_cap, const void *src, size_t src_sz) {
   size_t r = ZSTD_decompress(dst, dst_cap, src, src_sz);
-  return ZSTD_isError(r) ? 0 : r;
+  if (ZSTD_isError(r)) return 0;
+  return r;
 }
+
+size_t zstd_max_compressed_size(size_t src_sz) {
+  return ZSTD_compressBound(src_sz);
+}
+
