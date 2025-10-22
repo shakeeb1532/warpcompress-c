8a13ecb46b5e998bb35b96dc32ee25a3e101023c
shakeeb1532 <73624484+shakeeb1532@users.noreply.github.com>
2025-10-23 08:49:58 +1100

    Update codecs.h

--- include/codecs.h
+++ include/codecs.h
@@ -1,31 +1,22 @@
-#pragma once
+#ifndef CODECS_H
+#define CODECS_H
+
 #include <stddef.h>
 #include <stdint.h>
-#include <zstd.h>
 
-/* --- ZSTD (required) --- */
-typedef struct {
-  ZSTD_CCtx *cctx;
-} zstd_ctx_t;
+/* ---------- Zstd ---------- */
+size_t zstd_compress   (void *dst, size_t dst_cap, const void *src, size_t src_sz, int level);
+size_t zstd_decompress (void *dst, size_t dst_cap, const void *src, size_t src_sz);
+size_t zstd_max_compressed_size(size_t src_sz);
 
-zstd_ctx_t* zstd_ctx_create(int level, int nb_workers);
-void        zstd_ctx_free  (zstd_ctx_t* c);
-size_t      zstd_compress_ctx(zstd_ctx_t* c, void *dst, size_t dst_cap, const void *src, size_t src_sz);
-size_t      zstd_compress  (void *dst, size_t dst_cap, const void *src, size_t src_sz, int level);
-size_t      zstd_decompress(void *dst, size_t dst_cap, const void *src, size_t src_sz);
-size_t      zstd_max_compressed_size(size_t src_sz);
-
-/* --- LZ4 (optional) --- */
-#ifdef HAVE_LZ4
-size_t lz4_compress(void *dst, size_t dst_cap, const void *src, size_t src_sz);
-size_t lz4_decompress(void *dst, size_t dst_cap, const void *src, size_t src_sz);
+/* ---------- LZ4 (prefixed wrappers) ---------- */
+size_t wc_lz4_compress   (void *dst, size_t dst_cap, const void *src, size_t src_sz);
+size_t wc_lz4_decompress (void *dst, size_t dst_cap, const void *src, size_t src_sz);
 size_t lz4_max_compressed_size(size_t src_sz);
-#endif
 
-/* --- Snappy (optional) --- */
-#ifdef HAVE_SNAPPY
-size_t wc_snappy_compress  (void *dst, size_t dst_cap, const void *src, size_t src_sz);
-size_t wc_snappy_decompress(void *dst, size_t dst_cap, const void *src, size_t src_sz);
+/* ---------- Snappy (prefixed wrappers) ---------- */
+size_t wc_snappy_compress   (void *dst, size_t dst_cap, const void *src, size_t src_sz);
+size_t wc_snappy_decompress (void *dst, size_t dst_cap, const void *src, size_t src_sz);
 size_t snappy_max_compressed_size(size_t src_sz);
-#endif
 
+#endif /* CODECS_H */
