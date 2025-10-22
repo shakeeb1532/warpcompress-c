1a5516c31e929323e734d2eb3cee92166874d3c5
shakeeb1532 <73624484+shakeeb1532@users.noreply.github.com>
2025-10-23 08:52:00 +1100

    Update codecs_snappy.c

--- src/codecs_snappy.c
+++ src/codecs_snappy.c
@@ -1,23 +1,35 @@
 #include "codecs.h"
 #ifdef HAVE_SNAPPY
-#include <snappy-c.h>
-#include <string.h>
+#  include <snappy-c.h>
+#endif
+
+size_t snappy_max_compressed_size(size_t src_sz) {
+#ifdef HAVE_SNAPPY
+  return (size_t)snappy_max_compressed_length(src_sz);
+#else
+  (void)src_sz; return 0;
+#endif
+}
 
 size_t wc_snappy_compress(void *dst, size_t dst_cap, const void *src, size_t src_sz) {
+#ifdef HAVE_SNAPPY
   size_t out_len = dst_cap;
-  snappy_status st = snappy_compress((const char*)src, (size_t)src_sz, (char*)dst, &out_len);
-  return (st == SNAPPY_OK) ? out_len : 0;
+  snappy_status st = snappy_compress((const char*)src, src_sz, (char*)dst, &out_len);
+  return st == SNAPPY_OK ? out_len : 0;
+#else
+  (void)dst; (void)dst_cap; (void)src; (void)src_sz; return 0;
+#endif
 }
 
 size_t wc_snappy_decompress(void *dst, size_t dst_cap, const void *src, size_t src_sz) {
+#ifdef HAVE_SNAPPY
   size_t out_len = dst_cap;
-  snappy_status st = snappy_uncompress((const char*)src, (size_t)src_sz, (char*)dst, &out_len);
-  return (st == SNAPPY_OK) ? out_len : 0;
+  snappy_status st = snappy_uncompress((const char*)src, src_sz, (char*)dst, &out_len);
+  return st == SNAPPY_OK ? out_len : 0;
+#else
+  (void)dst; (void)dst_cap; (void)src; (void)src_sz; return 0;
+#endif
 }
 
-size_t snappy_max_compressed_size(size_t src_sz) {
-  return snappy_max_compressed_length((size_t)src_sz);
-}
-#endif
 
 
